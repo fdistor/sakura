5a316ed35a90e8599cb8b0ccd5970e5fc74d8315
fdistor <francis.distor@gmail.com>
2019-08-28 12:36:33 -0700

    posting message to parent but failing to resolve promises

--- src/workers/search.js
+++ src/workers/search.js
@@ -1,2 +1,18 @@
 const { Worker, parentPort, workerData } = require('worker_threads');
 const fs = require('fs');
+const filePath = __dirname + '/../../data/data.txt';
+
+const data = workerData;
+
+const search = options => {
+	options.encoding = 'utf8';
+	const stream = fs.createReadStream(filePath, options);
+
+	stream.on('data', data => console.log(data));
+
+	return options;
+};
+
+const result = search(data);
+
+parentPort.postMessage(result);
