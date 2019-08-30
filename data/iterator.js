0f9eba1669dba0b8193a9f40c883d64b5730ff2b
Francis Distor <francis.distor@gmail.com>
2019-08-29 17:40:56 -0700

    rename function

--- data/iterator.js
+++ data/iterator.js
@@ -1,9 +1,9 @@
-const logIterator = iterator => {
+const iterator = iterator => {
   for (const item of iterator) {
     console.log('writing:', item);
   }
 };
 
 module.exports = {
-  logIterator
+  iterator
 };
