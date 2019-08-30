1dbc52efb0f199efe79a63a8bdcc83099a149f74
Francis Distor <francis.distor@gmail.com>
2019-08-29 17:03:00 -0700

    import stream function

--- data/stream.js
+++ data/stream.js
@@ -1,17 +1,30 @@
 const faker = require('faker');
 
-function* wordGenerator() {
+function* stream() {
+  let count = 0;
+
   while (true) {
-    yield faker.lorem.sentence(3, 5);
+    count++;
+
+    if (count % 1e4 << 0 === 0) {
+      let length = 9;
+      const left = (Math.random() * length) << 0;
+      const right = length - left;
+      yield faker.lorem.words(left) + ' FiCo ' + faker.lorem.words(right);
+    } else yield faker.lorem.words(10);
   }
 }
 
-const counterIterator = wordGenerator();
+// const counterIterator = stream();
 
-const logIterator = iterator => {
-  for (const item of iterator) {
-    console.log('writing:', item);
-  }
+// const logIterator = iterator => {
+//   for (const item of iterator) {
+//     console.log('writing:', item);
+//   }
+// };
+
+// logIterator(counterIterator);
+
+module.exports = {
+  stream
 };
-
-logIterator(counterIterator);
