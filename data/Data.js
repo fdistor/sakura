10c7212b0c6da650b106c101cfe3706093d36547
Francis Distor <francis.distor@gmail.com>
2019-09-05 01:38:17 -0700

    remove unused functions

--- data/Data.js
+++ data/Data.js
@@ -5,35 +5,6 @@ module.exports = class StreamData {
     this.didWorkersFinish = false;
   }
 
-  *stream() {
-    let count = 0;
-
-    while (!this.didWorkersFinish) {
-      count++;
-
-      if (count % 1 << 0 === 0) {
-        let length = 9;
-        let left = this.rng(10);
-        let right = length - left;
-        const wordArray = [];
-
-        while (left > 0) {
-          wordArray.push(faker.lorem.word());
-          left--;
-        }
-
-        wordArray.push('FiCo');
-
-        while (right > 0) {
-          wordArray.push(faker.lorem.word());
-          right--;
-        }
-
-        yield wordArray.join(' ');
-      } else yield faker.lorem.words(10);
-    }
-  }
-
   data() {
     const random = this.rng(100);
     if (random === 0) {
@@ -55,10 +26,6 @@ module.exports = class StreamData {
       }
       return wordArray.join(' ');
     } else return faker.lorem.words(10);
-  }
-
-  stop() {
-    this.didWorkersFinish = true;
   }
 
   rng(max) {
