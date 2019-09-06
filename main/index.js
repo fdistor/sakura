2c8c513e435c5180aa7ab58545743f6ece0e5cef
Francis Distor <francis.distor@gmail.com>
2019-09-06 00:02:11 -0700

    fix -h log

--- main/index.js
+++ main/index.js
@@ -34,7 +34,7 @@ const execute = () => {
   if (arg) {
     if (arg === '-h')
       console.log(
-        'To start the program, at the root of the project, run `sakura [timeout]` where [timeout] is an integer in milliseconds that determines how long the program will run.\n No integer input will default to 60000ms.'
+        'To start the program, run `sakura [timeout]` where [timeout] is an integer in milliseconds that determines how long the program will run.\n No integer input will default to 60000ms.'
       );
     else run(arg);
   } else run(60000);
