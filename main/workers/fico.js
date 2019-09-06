56ec0d5e5f28cf1fa61ac00443adf9f52020a9dc
Francis Distor <francis.distor@gmail.com>
2019-09-06 00:39:25 -0700

    implement kmp

--- main/workers/fico.js
+++ main/workers/fico.js
@@ -4,14 +4,42 @@ const countBytes = string => {
   return encodeURI(string).split(/%..|./).length - 1;
 };
 
+const kmp = text => {
+  const patternTable = [0, 0, 0, 0];
+  const word = 'FiCo';
+  let textIndex = 0;
+  let wordIndex = 0;
+
+  while (textIndex < text.length) {
+    if (text[textIndex] === word[wordIndex]) {
+      if (wordIndex === word.length - 1) {
+        return textIndex - word.length + 1;
+      }
+
+      wordIndex += 1;
+      textIndex += 1;
+    } else if (wordIndex > 0) {
+      wordIndex = patternTable[wordIndex - 1];
+    } else {
+      wordIndex = 0;
+      textIndex += 1;
+    }
+  }
+
+  return -1;
+};
+
 parentPort.on('message', message => {
   const result = {};
-  const bytes = countBytes(message.chunk);
+  const search = kmp(message.chunk);
+  let bytes = 0;
 
-  if (message.chunk === 'FiCo') {
-    result.didFind = true;
-  } else {
+  if (search === -1) {
     result.didFind = false;
+    bytes = countBytes(message.chunk);
+  } else {
+    result.didFind = true;
+    bytes = countBytes(message.chunk.slice(0, search + 4));
   }
 
   result.id = message.id;
